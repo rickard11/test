996a207a0f12a6e03ac27ad06d49934b49ff303b
rickard11 <rickard@ucsb.edu>
2024-02-12 17:01:49 -0800

    edited

--- TSS data cleaning.R
+++ TSS data cleaning.R
@@ -1,9 +1,8 @@
 #First download needed csv's and libraries
-setwd("C:/Users/rickard/Documents/2024 Nutrient and TSS analysis")
 library(readxl)
 library(ggplot2)
-TSS<-read_xlsx("~/2024 Nutrient and TSS analysis/TSS_allyears.xlsx",sheet=3)
-sample_locations<-read_xlsx("~/2024 Nutrient and TSS analysis/TSS_allyears.xlsx",sheet=4)
+TSS<-read_xlsx("TSS_allyears.xlsx",sheet=3)
+sample_locations<-read_xlsx("TSS_allyears.xlsx",sheet=4)
 
 #remove negative values
 TSS<-TSS[TSS$`TSS (mg/L)`>0 & TSS$`TSS (mg/L)`!="see above",]
@@ -18,8 +17,33 @@ data<-merge(TSS,sample_locations,by="Sample Number",all.x=TRUE)
 data
 #Look for any columns with missing data
 
+
+#Remove a and b so that titles match
+TSS$`Sample Number`<-gsub("a","",as.character(TSS$`Sample Number`))
+TSS$`Sample Number`<-gsub("b","",as.character(TSS$`Sample Number`))
+data<-merge(TSS,sample_locations,by="Sample Number",all.x=TRUE)
+data
+
+#change Location names to match
+data$Location<-gsub("NCOS-P","Phelps",data$Location)
+data$Location<-gsub("NCOS-V","Venoco",data$Location)
+data$Location<-gsub("NCOS-D","Devereux",data$Location)
+data$Location<-gsub("NCOS-W","Whittier",data$Location)
+unique(data$Location)
+data
+?grepl
 getwd()
 
-install.packages("usethis")
+write.csv(data,"C:/Users/rickard/Documents/test/TSS_andlocations.csv")
 
-#Adding comment
+missingdata<-data[is.na(data$Location)==TRUE,]
+missingdata
+
+##Upload new file that has some 2020 data in it to reduce missing data sheet
+
+locations2020<-read_xlsx("NCOS_Stormwater_SSC_Data_WY2020.xlsx")
+locations2020
+locations2020<-locations2020[,1:4]
+
+missingdata<-merge(missingdata,locations2020,by="Sample Number",all.x=TRUE)
+missingdata
