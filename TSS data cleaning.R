a1b2916afa61c9e85c714f490f67b83dc55094c5
rickard11 <100870004+rickard11@users.noreply.github.com>
2024-02-13 09:40:46 -0800

    added 2021 missing data

--- TSS data cleaning.R
+++ TSS data cleaning.R
@@ -42,8 +42,38 @@ missingdata
 ##Upload new file that has some 2020 data in it to reduce missing data sheet
 
 locations2020<-read_xlsx("NCOS_Stormwater_SSC_Data_WY2020.xlsx")
+colnames(locations2020)
+colnames(locations2020)<-c("Sample Number","Location","Date","Time")
 locations2020
 locations2020<-locations2020[,1:4]
-
-missingdata<-merge(missingdata,locations2020,by="Sample Number",all.x=TRUE)
+colnames(locations2020)
+colnames(missingdata)
+missingdata<-merge(missingdata,locations2020,by=c("Sample Number"),all.x=TRUE)
 missingdata
+
+
+##Load 2021 data
+
+data2021<-read_xlsx("NCOS_Stormwater_Sample_Log_WY2021.xlsx")
+colnames(data2021)
+unique(data2021$Notes)
+data2021<-data2021[,c(1:3,5:6)]
+colnames(data2021)
+colnames(data2021)<-c("Location","Sample Type","Date","Time","Sample Number")
+colnames(data)
+data2021
+missingdata<-merge(missingdata,data2021,by=c("Sample Number"),all.x=TRUE)
+locations2020$`Sample Type`<-NA
+
+
+
+#Merge everything
+colnames(data2021)
+colnames(locations2020)
+colnames(data)
+allmissingdata<-rbind(data2021,locations2020)
+allmissingdata
+
+
+
+
